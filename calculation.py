3cb3dba4a245ed92a21bb8dde49965e732511878
cedricwang <cedric.wb@gmail.com>
2018-12-16 16:35:31 +0800

    shiyan1

---
+++ calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+a = 1
+sum = 0
+
+while a <= 100:
+    sum = a + sum
+    a += 1
+print('1 add to 100,sum is',sum)
---
+++ newargtest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+import sys
+
+for inpt in sys.argv[1:]:
+    if  len(inpt) > 3:
+        print(inpt)
